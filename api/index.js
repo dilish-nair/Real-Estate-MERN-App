78325f45eacaf7cbb8673f0265d54a7b28c31913
Dilish N Nair <dilishnair1979@gmail.com>
2024-07-26 20:44:03 +0530

    Connected database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,13 @@
 import express from 'express'
+import mongoose from'mongoose'
+import dotenv from 'dotenv'
+dotenv.config();
+
+
+mongoose.connect(process.env.MONGO, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Error connecting to MongoDB:', err));
+
 
 const app = express();
 
